e677d82867e8e1a4aa68c9ade228057064757892
dwayneosmith <78266064+dwayneosmith@users.noreply.github.com>
2022-02-12 12:56:35 +0530

    first commit

---
+++ hextodec.js
@@ -0,0 +1,6 @@
+function hexadecimaltodecimal() {
+    var user_input = document.getElementById("user_input").value;
+    var converttobig = new BigNumber(user_input , 16);
+    var answerconversionr = converttobig.toString(10);
+    document.getElementById("answer").innerHTML = answerconversionr;
+   }
